bc73355a3f15fcbe2942bbcf6ec410d4f27b02a1
Kaminuri <matt-picard@hotmail.fr>
2015-02-08 19:18:00 +0100

    Haters

--- Modelisation/src/frame/Datframe.java
+++ Modelisation/src/frame/Datframe.java
@@ -1,11 +1,14 @@
 package frame;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Datframe extends JFrame{
 	
 	public Datframe(){
-		
+		JLabel image = new JLabel( new ImageIcon( "src/pictures/wow.png"));
+		this.getContentPane().add(image );
 		this.setResizable(false);
 		this.pack();
 	    this.setLocationRelativeTo(null);
