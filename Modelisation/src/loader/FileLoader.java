77eb1d01fb7d699f51cc5348ddf945f503f0ac9c
Kaminuri <matt-picard@hotmail.fr>
2015-02-08 19:26:07 +0100

    maj

--- Modelisation/src/loader/FileLoader.java
+++ Modelisation/src/loader/FileLoader.java
@@ -19,7 +19,7 @@ public class FileLoader {
 		clearLists();
 		String sta;
 		String currnt;
-		try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Mathias\\Documents\\GitHub\\Modelisation\\Modelisation\\src\\models\\cone.gts"))){
+		try(BufferedReader br = new BufferedReader(new FileReader("src/models/cone.gts"))){
 			sta = br.readLine();
 			initNbs(sta);
 			for(int i = 0; i<nbPoint; i++){
