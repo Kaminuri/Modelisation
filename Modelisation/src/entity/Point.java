4158670a75bdd919ce147d2a3dbb532c168e161d
komanokami <komanokami@users.noreply.github.com>
2015-02-12 16:02:54 +0100

    Update Point.java
    Ajout getters

--- Modelisation/src/entity/Point.java
+++ Modelisation/src/entity/Point.java
@@ -7,6 +7,17 @@ public class Point {
 	public Point(double x, double y, double z){
 		c = new Coordonnees(x, y, z);
 	}
+	public double getX(){
+		return c.getX();
+	}
+	
+	public double getY(){
+		return c.getY();
+	}
+	
+	public double getZ(){
+		return c.getZ();
+	}
 	public String toString(){
 		return c + "";
 	}
