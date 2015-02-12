ca702cc2fa7e983c92a2567c59966767bd20e1a5
Clement Cattelle <gamisensei@gmail.com>
2015-02-12 18:51:41 +0100

    Creation de deux points pré-définis , et traçage d'un trait

--- Modelisation/src/panels/Panes.java
+++ Modelisation/src/panels/Panes.java
@@ -7,19 +7,26 @@ import javax.swing.JPanel;
 
 import publics.Const;
 import entity.Face;
+import entity.Point;
 import entity.Segment;
 
 public class Panes extends JPanel{
 	ArrayList<Segment> s;
+	Point a;
+	Point b;
 	Segment p;
 	public Panes(){
-		s = Const.segments;
-		p = s.get(0);
+		a = new Point(5, 10, 0);
+		b = new Point(10, 30, 0);
+		p = new Segment(a, b);
+		/*s = Const.segments;
+		p = s.get(0);*/
 	}
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		/*for(Face r : f){
 			g.drawPolygon(r.x, arg1, arg2);
 		}*/
+		g.drawLine((int)a.getX(),(int)a.getY(), (int)b.getX(), (int)b.getY());
 	}
 }
