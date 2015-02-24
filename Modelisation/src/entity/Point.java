2bca462dbeee3bfae6c21d58db138322cff5d77f
Kaminuri <matt-picard@hotmail.Fr>
2015-02-24 15:42:12 +0100

    scalarProductAdd

--- Modelisation/src/entity/Point.java
+++ Modelisation/src/entity/Point.java
@@ -7,7 +7,11 @@ public class Point {
 	public Point(double x, double y, double z){
 		c = new Coordonnees(x, y, z);
 	}
-	
+	public int scalarProduct(Point p){
+		return this.getY()*p.getZ() - p.getY()*this.getZ() 
+		+ this.z*p.getX()- this.x*p.getZ() 
+		+ this.getX()*p.getY() - this.getX()*p.getY();
+	}
 	public void multiply(int m){
 		c = new Coordonnees(c.getX()*m, c.getY()*m, c.getZ()*m);
 	}
