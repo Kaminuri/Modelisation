8904ddab4ce0400f364b652bcd976e357e0094d1
Hodapp-Jeremy <jeje59730@free.fr>
2015-02-26 15:09:14 +0100

    Class de Teste

---
+++ Modelisation/src/teste/TestePublics.java
@@ -0,0 +1,51 @@
+package teste;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import publics.Coordonnees;
+
+public class TestePublics {
+	
+	Coordonnees c = new Coordonnees(5,5,5);
+	
+	@Test
+	public void testAjouterX() {
+		Coordonnees x = new Coordonnees(7,7,7);
+		c.ajouter(x);
+		Coordonnees v = new Coordonnees(12,12,12);
+		assertEquals(v.getX(),c.getX(),1);
+	}
+	
+	@Test
+	public void testAjouterY() {
+		Coordonnees x = new Coordonnees(7,7,7);
+		c.ajouter(x);
+		Coordonnees v = new Coordonnees(12,12,12);
+		assertEquals(v.getY(),c.getY(),1);
+	}
+	
+	@Test
+	public void testAjouterZ() {
+		Coordonnees x = new Coordonnees(7,7,7);
+		c.ajouter(x);
+		Coordonnees v = new Coordonnees(12,12,12);
+		assertEquals(v.getZ(),c.getZ(),1);
+	}
+	
+	@Test
+	public void testEqualsP() {
+		Coordonnees x = new Coordonnees(5,5,5);
+		boolean b = c.equals(x);
+		assertEquals(b,true);
+	}
+	
+	@Test
+	public void testEqualsN() {
+		Coordonnees x = new Coordonnees(8,8,8);
+		boolean b = c.equals(x);
+		assertEquals(b,false);
+	}
+	
+}
