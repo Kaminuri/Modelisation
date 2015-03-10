a757b8efeeb1cf76bec3a4d7deb0c37b93068c30
Clement Cattelle <gamisensei@gmail.com>
2015-03-10 16:45:28 +0100

    Ajout CompareTo

--- Modelisation/src/entity/Point.java
+++ Modelisation/src/entity/Point.java
@@ -1,42 +1,79 @@
 package entity;
 
+import java.util.Comparator;
+
 import publics.Coordonnees;
 
-public class Point {
+public class Point implements Comparable<Point>,Comparator<Point>{
 	private Coordonnees c;
 	public Point(double x, double y, double z){
 		c = new Coordonnees(x, y, z);
 	}
+	public Point(){
 	
+	}
 	public double scalarProduct(Point p){
 		return c.getX()*p.getX()+c.getY()*p.getY()+c.getZ()*p.getZ();
 	}
-	
+
 	public void multiply(int m){
 		c = new Coordonnees(c.getX()*m, c.getY()*m, c.getZ()*m);
 	}
-	
+
 	public double getX(){
 		return c.getX();
 	}
-	
+
 	public double getY(){
 		return c.getY();
 	}
-	
+
 	public double getZ(){
 		return c.getZ();
 	}
-	
+
 	public String toString(){
 		return c + "";
 	}
-	
+
 	public boolean equals(Point p){
 		return c.equals(p.getC());
 	}
-	
+
 	private Coordonnees getC() {
 		return c;
+	}
+
+	@Override
+	public int compareTo(Point p) {
+		if(p.getZ()<this.getZ()){
+			return 1;
+		}
+		else if(p.getZ()>this.getZ()){
+			return -1;
+		}
+		else if(p.getY() < this.getY()){
+			return 1;
+		}
+		else if(p.getY() > this.getY()){
+			return -1;
+		}
+		return 0;
+	}
+	@Override
+	public int compare(Point p1, Point p2) {
+		if(p1.getZ()<p2.getZ()){
+			return 1;
+		}
+		else if(p1.getZ()>p2.getZ()){
+			return -1;
+		}
+		else if(p1.getY() < p2.getY()){
+			return 1;
+		}
+		else if(p1.getY() > p2.getY()){
+			return -1;
+		}
+		return 0;
 	}
 }
