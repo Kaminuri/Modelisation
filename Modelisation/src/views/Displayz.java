40168bdf54c2c3bbbc0da5c80d496c819d6626f6
Clement Cattelle <gamisensei@gmail.com>
2015-03-10 16:46:19 +0100

    Reprise de la classe d'Eric

--- Modelisation/src/views/Displayz.java
+++ Modelisation/src/views/Displayz.java
@@ -1,28 +1,36 @@
 package views;
 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.Line2D;
+import java.util.Collection;
+import java.util.Collections;
 
-import javax.swing.JDesktopPane;
+import javax.swing.JPanel;
 
+import entity.Point;
 import loader.FileLoader;
 import publics.Const;
-import entity.Segment;
 
-public class Displayz extends JDesktopPane{
 
-	//Chacal !
+public class Displayz extends JPanel {
+	
+	
+
+	private static final long serialVersionUID = 1L;
 
 	public Displayz(){
 		FileLoader f = new FileLoader();
+		Collections.sort(Const.points,new Point());
 	}
-	
-	@Override
-	public void paintComponent(Graphics g){
-		Graphics2D g2 = (Graphics2D) g;
-		for (Segment seg : Const.segments) {
-			g2.draw(new Line2D.Double(seg.getP1().getX(), seg.getP1().getY(), seg.getP2().getX(), seg.getP2().getY()));
-		}
-	}
+		public void paintComponent(Graphics g){
+			int [] listeX = new int [Const.points.size()];
+			int [] listeY = new int[Const.points.size()];
+			double x0 = Const.screenSize.getWidth() / 2;
+			double y0 = Const.screenSize.getHeight() / 2;
+			for(int i = 0; i < Const.points.size(); i++){
+				listeX[i] = (int)(Const.points.get(i).getX()*100+x0);
+				listeY[i] = (int)(Const.points.get(i).getY()*(-100) + y0);
+			}
+			g.drawPolygon(listeX, listeY, Const.points.size());
+
+		  }               
 }
