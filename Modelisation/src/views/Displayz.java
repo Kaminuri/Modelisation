9fdb4c9ee1a69fae3924a5bda1d321adf9d578ed
Clement Cattelle <gamisensei@gmail.com>
2015-03-22 19:23:06 +0100

    Remplacage des int par les valeurs dans coordonnees.java

--- Modelisation/src/views/Displayz.java
+++ Modelisation/src/views/Displayz.java
@@ -8,29 +8,33 @@ import javax.swing.JPanel;
 
 import entity.Point;
 import loader.FileLoader;
+import panels.OptionPanel;
 import publics.Const;
+import publics.Coordonnees;
 
 
 public class Displayz extends JPanel {
-	
-	
 
+
+	
 	private static final long serialVersionUID = 1L;
 
 	public Displayz(){
 		FileLoader f = new FileLoader();
 		Collections.sort(Const.points,new Point());
 	}
-		public void paintComponent(Graphics g){
-			int [] listeX = new int [Const.points.size()];
-			int [] listeY = new int[Const.points.size()];
-			double x0 = Const.screenSize.getWidth() / 2;
-			double y0 = Const.screenSize.getHeight() / 2;
-			for(int i = 0; i < Const.points.size(); i++){
-				listeX[i] = (int)(Const.points.get(i).getX()*100+x0);
-				listeY[i] = (int)(Const.points.get(i).getY()*(-100) + y0);
-			}
-			g.drawPolygon(listeX, listeY, Const.points.size());
+	public void paintComponent(Graphics g){
+		int [] listeX = new int [Const.points.size()];
+		int [] listeY = new int[Const.points.size()];
+		double x0 = Const.screenSize.getWidth() / 2;
+		double y0 = Const.screenSize.getHeight() / 2;
+		for(int i = 0; i < Const.points.size(); i++){
+			listeX[i] = (int)(Const.points.get(i).getX()*Coordonnees.getTransX() + x0);
+			listeY[i] = (int)(Const.points.get(i).getY()*Coordonnees.getTransY() + y0);
+		}
+		g.drawPolygon(listeX, listeY, Const.points.size());
 
-		  }               
+	}    
+	
+	
 }
