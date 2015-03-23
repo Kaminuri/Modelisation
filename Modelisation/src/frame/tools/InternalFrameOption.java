6aa2be58db46b8676bbd723012f0ad6e76b0ff1f
Clement Cattelle <gamisensei@gmail.com>
2015-03-23 19:56:13 +0100

    Ajout Displayz dans le constructeur

--- Modelisation/src/frame/tools/InternalFrameOption.java
+++ Modelisation/src/frame/tools/InternalFrameOption.java
@@ -4,12 +4,13 @@ import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 
 import panels.OptionPanel;
+import views.Displayz;
 
 public class InternalFrameOption extends JInternalFrame{
 	
-	public InternalFrameOption(){
+	public InternalFrameOption(Displayz d){
 		super("",false,true,false, true);
-		OptionPanel op = new OptionPanel();
+		OptionPanel op = new OptionPanel(d);
 		setContentPane(op);
 		setSize(240,70);
 		setLocation(50, 50);
