2abf1d5882bfefd3b2e28004c8293bdb504352b8
Clement Cattelle <gamisensei@gmail.com>
2015-04-07 17:22:15 +0200

    Suppression des attributs , déplacés dans Displayz

--- Modelisation/src/publics/Coordonnees.java
+++ Modelisation/src/publics/Coordonnees.java
@@ -1,10 +1,8 @@
 package publics;
 
+
 public class Coordonnees {
 	private double x, y, z;
-	private static int zoomX = 100;
-	private static int zoomY = -100;
-	private static int transX,transY,transZ;
 
 	public Coordonnees(double x1,double y1,double z2){
 		x = x1;
@@ -33,49 +31,8 @@ public class Coordonnees {
 		return x ==c.x && y == c.y && z == c.z;
 	}
 
-	public static void increaseZoomX(){
-		zoomX= zoomX + 2;
-	}
 
-	public static void decreaseZoomX(){
-		zoomX= zoomX - 2;
-	}
-
-	public static void increaseZoomY(){
-		zoomY = zoomY + 2;
-	}
-
-	public static void decreaseZoomY(){
-		zoomY = zoomY - 2;
-	}
-	public static int getZoomX() {
-		return zoomX;
-	}
-	public static int getZoomY() {
-		return zoomY;
-	}
 	
-	public static int increaseTransX(){
-		return transX++;
-	}
-	public static int increaseTransY(){
-		return transY++;
-	}
-	public static int decreaseTransX(){
-		return transX--;
-	}
-	public static int decreaseTransY(){
-		return transY--;
-	}
-	public static int getTransX(){
-		return transX;
-	}
-
-	public static int getTransY(){
-		return transY;
-	}
-	public static int getTransZ() {
-		return transZ;
-	}
+	
 
 }
