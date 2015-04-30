249c2be716637ca05c126558410568a8ae0d3f09
Lefebvre Eric <eric.lefebvre59@orange.fr>
2015-04-30 18:46:45 +0200

    Import

--- Modelisation/src/frame/tools/FileInsert.java
+++ Modelisation/src/frame/tools/FileInsert.java
@@ -1,28 +1,38 @@
 package frame.tools;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import BDD.Base;
+
 public class FileInsert extends JFrame{
-	private File f = new File("src/resources/models/cone.gts");
+	protected File f;
+	protected Base bdd;
 	private FileInsert lll;
-	public FileInsert(File f){
+	public FileInsert(final File f){
+		bdd = new Base();
 		lll = this;
 		this.f = f;
 		JPanel contain = new JPanel();
 		//Label
 		JPanel labelContainer = new JPanel();
 		labelContainer.setLayout(new BoxLayout(labelContainer, BoxLayout.Y_AXIS));
-		JTextField name = new JTextField(f.getName());
+		final JTextField name = new JTextField(f.getName());
 		JLabel tag = new JLabel("Tags");
 		JLabel desc = new JLabel("Description");
 		labelContainer.add(name);
@@ -71,8 +81,8 @@ public class FileInsert extends JFrame{
 				
 			}
 		});
-		JTextField non = new JTextField();
-		JTextField tg = new JTextField();
+		final JTextField non = new JTextField();
+		final JTextField tg = new JTextField(" ");
 		textContainer.add(buttonToChangeFile);
 		textContainer.add(non);
 		textContainer.add(tg);
@@ -81,6 +91,51 @@ public class FileInsert extends JFrame{
 		JPanel buttonContainer = new JPanel();
 		buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.Y_AXIS));
 		JButton next = new JButton("Next");
+		next.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String tags = non.getText();
+				String nam = name.getText();
+				String des =  tg.getText();
+				String[] tab = tags.split(",");
+				ArrayList<String> tab2 = new ArrayList<String>() ;
+				if(!bdd.estDeja(nam)){
+					File dest = new File("src\\resources\\models\\" + nam + ".gts");
+					copyFile(f, dest);
+					if(!tg.equals(" ")){
+						tab2.add(nam);						
+						if(tab.length >= 3){
+							for(int i = 1; i < tab.length; i++){
+							tab2.add(tab[i-1]);
+							}
+						}
+						else if(tab.length < 3){
+							int i = 1;
+							for(; i < tab.length + 1; i++){
+								tab2.add(tab[i-1]);
+							}
+							for(; i < 4; i++){
+								tab2.add("null");
+							}
+						}
+					}
+					else{
+						tab2.add(nam);
+						for(int i = 1; i < 3; i ++){
+							tab2.add("null");
+						}
+					}
+					bdd.insert(nam, nam + ".gts", tab2.get(0), tab2.get(1), tab2.get(2), tab2.get(3), 0, 0, 0);
+					
+				}
+					
+				else {
+					JOptionPane.showMessageDialog(new JFrame(), "Le nom existe deja");
+				}
+								
+			}
+		}); 
 		JButton cancel = new JButton("Cancel");
 		cancel.addMouseListener(new MouseListener() {
 			
@@ -126,5 +181,37 @@ public class FileInsert extends JFrame{
 		this.pack();
 	    this.setLocationRelativeTo(null);
 	    this.setVisible(true);
+	}
+	
+	public static boolean copyFile(File source, File dest){
+		try{
+			// Declaration et ouverture des flux
+			java.io.FileInputStream sourceFile = new java.io.FileInputStream(source);
+	 
+			try{
+				java.io.FileOutputStream destinationFile = null;
+	 
+				try{
+					destinationFile = new FileOutputStream(dest);
+	 
+					// Lecture par segment de 0.5Mo 
+					byte buffer[] = new byte[512 * 1024];
+					int nbLecture;
+	 
+					while ((nbLecture = sourceFile.read(buffer)) != -1){
+						destinationFile.write(buffer, 0, nbLecture);
+					}
+				} finally {
+					destinationFile.close();
+				}
+			} finally {
+				sourceFile.close();
+			}
+		} catch (IOException e){
+			e.printStackTrace();
+			return false; // Erreur
+		}
+	 
+		return true; // Résultat OK  
 	}
 }
